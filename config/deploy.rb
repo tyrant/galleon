24a9a5e5242620eb100ce2e6da0bdd00706a693f
Mikey Clarke <epicschnozz@gmail.com>
2017-12-07 23:09:56 +1300

    Added master.key to Capistrano linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/app-user/galleon"
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "public/system"#, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
